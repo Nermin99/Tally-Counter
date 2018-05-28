0ba5164d42b0605782a9fa416f5e020e808ff95b
Nermin Skenderovic <nermin.skenderovic.1999@gmail.com>
2018-05-28 22:59:14 +0200

    removed functions due to not necessary and easier to store with JSON

--- app/classes/Substance.js
+++ app/classes/Substance.js
@@ -1,29 +1,11 @@
 class Substance {
   constructor(name, id, keyCombo = null, zoom = "100x") {
-    this.id = id;
-    this.name = name;
     this.counter = 0;
-    if (keyCombo) this.setKeyCombo(keyCombo);
+    this.id = id;
+    this.keyCombo = keyCombo;
+    this.name = name;
     this.zoom = zoom;
   }
-
-  increment() {
-    this.counter++;
-  };
-
-  decrement() {
-    if (this.counter == 0) return;
-    this.counter--;
-  };
-
-  setKeyCombo(keyCombo) {
-    this.keyCombo = keyCombo;
-  }
-
-  setZoom(zoom) {
-    this.zoom = zoom
-  }
-
 }
 
 // export default Substance;
